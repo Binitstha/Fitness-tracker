3af4eb361ef661103706db5c637d4b44408b805d
Binitstha <binitcr336@gmail.com>
2024-07-08 18:09:53 +0545

    error fixed for date picker

--- Frontend/src/app/auth/signup/page.tsx
+++ Frontend/src/app/auth/signup/page.tsx
@@ -64,8 +64,8 @@ const Signin = () => {
       email: "",
       password: "",
       dateOfBirth: undefined,
-      gender: "",
-      country: "",
+      gender: undefined,
+      country: undefined,
     },
   });
 
@@ -152,37 +152,14 @@ const Signin = () => {
                 render={({ field }) => (
                   <FormItem className="flex flex-col">
                     <FormLabel>Date of Birth</FormLabel>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <FormControl>
-                          <Button
-                            variant={"outline"}
-                            className={cn(
-                              " pl-3 text-left font-normal",
-                              !field.value && "text-muted-foreground",
-                            )}
-                          >
-                            {field.value ? (
-                              format(field.value, "PPP")
-                            ) : (
-                              <span>Pick a date</span>
-                            )}
-                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                          </Button>
-                        </FormControl>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          selected={field.value}
-                          onSelect={field.onChange}
-                          disabled={(date) =>
-                            date > new Date() || date < new Date("1900-01-01")
-                          }
-                          initialFocus
-                        />
-                      </PopoverContent>
-                    </Popover>
+                    <div className="">
+                      <Input
+                        placeholder="mm/dd/yyyy"
+                        type="date"
+                        min="1900-01-01"
+                        max={new Date().toISOString().split("T")[0]}
+                      />
+                    </div>
                     <FormMessage />
                   </FormItem>
                 )}
