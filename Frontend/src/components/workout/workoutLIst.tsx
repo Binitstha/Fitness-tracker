1b63f7e97c22bb66e3e147dbfe184f2b5ef2a5a7
Binitstha <binitcr336@gmail.com>
2024-07-24 18:57:33 +0545

    feat: Add alert dialog for workout deletion confirmation in WorkoutList component

--- Frontend/src/components/workout/workoutLIst.tsx
+++ Frontend/src/components/workout/workoutLIst.tsx
@@ -1,3 +1,14 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -37,7 +48,26 @@ const WorkoutList = () => {
               </CardContent>
               <CardFooter className="flex justify-between">
                 <Button variant="outline">Update</Button>
-                <Button variant="destructive">Delete</Button>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="destructive">Delete</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Are you absolutely sure?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        This action cannot be undone. This will permanently
+                        delete your workout data from our servers.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
               </CardFooter>
             </Card>
           </CarouselItem>
