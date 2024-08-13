fd0513f7b9b2e18175993131e84b44c49b40ed14
Binitstha <binitcr336@gmail.com>
2024-08-13 23:53:59 +0545

    fix: Multer error fixed for image

--- Backend/src/routes/blog/blog.routes.ts
+++ Backend/src/routes/blog/blog.routes.ts
@@ -3,13 +3,15 @@ import { authenticationMiddleware } from "../../middleware/authentication";
 import { validateSchema } from "../../middleware/validateSchema";
 import { commentSchema, postSchema } from "../../schemas/schemas";
 import { postComment, postContent } from "./blog.controller";
+import upload from "../../config/multer.config";
 
 const router = Router();
 
 router.post(
   "/addBlog",
-  // authenticationMiddleware,
-  // validateSchema(postSchema),
+  authenticationMiddleware,
+  upload.single("image"),
+  validateSchema(postSchema),
   postContent
 );
 
