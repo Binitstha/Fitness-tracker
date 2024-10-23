2bcfa62beb59a20b9df1c24138abc329da0e2653
Binitstha <binitcr336@gmail.com>
2024-10-23 21:55:35 +0545

    fix: react hook dependency fixed

--- Frontend/src/context/authContext.tsx
+++ Frontend/src/context/authContext.tsx
@@ -58,7 +58,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     } finally {
       setLoading(false);
     }
-  }, []);
+  }, [Url]);
 
   const logOut = useCallback(async () => {
     try {
@@ -87,12 +87,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     } catch (error) {
       console.error("Failed to log out:", error);
     }
-  }, []);
+  }, [Url]);
 
   useEffect(() => {
     fetchUserDetails();
   }, [fetchUserDetails]);
-  
+
   if (user) {
     return (
       <AuthContext.Provider
