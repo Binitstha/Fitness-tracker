a2bc16336da416b794ecc3087658a845e827a4a3
Binitstha <binitcr336@gmail.com>
2025-02-09 15:24:47 +0545

    fix: update remote image patterns in Next.js config to include production hostname

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost",
-          port: "5000",
-        },
-      ],
-    },
-  };
-  
-  // Use module.exports instead of ES modules export
-  export default nextConfig;
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+      },
+      {
+        protocol: "https",
+        hostname: "https://fitness-tracker-3-o8ue.onrender.com",
+      },
+    ],
+  },
+};
+
+// Use module.exports instead of ES modules export
+export default nextConfig;
